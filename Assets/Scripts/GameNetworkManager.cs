8654ec521e102bf4d87aef2d7612dcb71125fc8a
Oluwatosin-Ogunyebi <oluwatosinogunyebi@gmail.com>
2023-08-25 03:30:02 +0100

    Added Relay Service Updates
     Updated the Game Network Manager

--- Assets/Scripts/GameNetworkManager.cs
+++ Assets/Scripts/GameNetworkManager.cs
@@ -18,6 +18,7 @@ using NetworkEvent = Unity.Networking.Transport.NetworkEvent;
 
 using TMPro;
 using System.Threading.Tasks;
+using Unity.VisualScripting;
 
 public class GameNetworkManager : MonoBehaviour
 {
@@ -35,6 +36,26 @@ public class GameNetworkManager : MonoBehaviour
     private bool _clientAuthenticated = false;
     private string _joinCode;
 
+    private async void Start()
+    {
+        await AuthenticatePlayer();
+    }
+    async Task AuthenticatePlayer()
+    {
+        try
+        {
+            await UnityServices.InitializeAsync(); //Initalize Any Asynchronous Task
+            await AuthenticationService.Instance.SignInAnonymouslyAsync(); //Sign in the user anonymously
+            _playerID = AuthenticationService.Instance.PlayerId;
+            _clientAuthenticated = true;
+            _playerIDText.text = $"Client Authentication Successful - {_playerID}";
+        }
+        catch (Exception e)
+        {
+
+            Debug.Log(e);
+        }
+    }
     public async Task<RelayServerData> AllocateRelayServerAndGetCode(int maxConnections, string region = null)
     {
         Allocation allocation;
@@ -64,20 +85,134 @@ public class GameNetworkManager : MonoBehaviour
         return new RelayServerData(allocation, "dtls");
     }
 
+
+    public async Task<RelayServerData> JoinRelayServerWithCode(string joinCode)
+    {
+        JoinAllocation allocation;
+
+        try
+        {
+            allocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+        }
+        catch (Exception e)
+        {
+            Debug.Log($"Relay allocation join request failed - {e}");
+            throw;
+        }
+
+        Debug.Log($"Client: {allocation.ConnectionData[0]} {allocation.ConnectionData[1]}");
+        Debug.Log($"Host: {allocation.HostConnectionData[0]} {allocation.HostConnectionData[1]}");
+        Debug.Log($"Client: {allocation.AllocationId}");
+
+        return new RelayServerData(allocation, "dtls");
+    }
+
+    IEnumerator ConfigureGetCodeAndJoinHost()
+    {
+        //Run the task to get code and join as host
+        var allocateAndGetCode = AllocateRelayServerAndGetCode(_maxConnections);
+
+        while(!allocateAndGetCode.IsCompleted)
+        {
+            //Wait until we create the allocation and get code
+            yield return null;
+        }
+
+        //Show allocation error if allocation and code is not gotten
+        if (allocateAndGetCode.IsFaulted)
+        {
+            Debug.LogError($"Cannot start the server due to an exception {allocateAndGetCode.Exception.Message}");
+            yield break;
+        }
+
+        var relayServerData = allocateAndGetCode.Result;
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+        NetworkManager.Singleton.StartHost();
+
+        //UI Updates
+        _joinCodeText.gameObject.SetActive(true);
+        _joinCodeText.text = _joinCode;
+        _statusTxt.text = "Joined as Host";
+    }
+
+    IEnumerator ConfigureUseCodeAndJoinClient(string joinCode)
+    {
+        var joinAllocationFromCode = JoinRelayServerWithCode(joinCode);
+
+        while (!joinAllocationFromCode.IsCompleted)
+        {
+            //Wait until we create the allocation and get code
+            yield return null;
+        }
+
+        //Show allocation error if allocation and code is not gotten
+        if (joinAllocationFromCode.IsFaulted)
+        {
+            Debug.LogError($"Cannot join the relay server due to an exception {joinAllocationFromCode.Exception.Message}");
+            yield break;
+        }
+
+        var relayServerData = joinAllocationFromCode.Result;
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+        NetworkManager.Singleton.StartClient();
+
+        //UI Updates
+        _statusTxt.text = "Joined as Client";
+    }
     public void JoinHost()
     {
-        NetworkManager.Singleton.StartHost();
-        _statusTxt.text = "Joined as Host";
+        if (!_clientAuthenticated)
+        {
+            Debug.Log("Client not Authenticated. Please Try Again");
+            return;
+        }
+
+        StartCoroutine(ConfigureGetCodeAndJoinHost());
+
+        //UIUpdates
+        _btnClient.gameObject.SetActive(false);
+        _btnHost.gameObject.SetActive(false);
+        _joinCodeText.gameObject.SetActive(false);
+
+
+        //NetworkManager.Singleton.StartHost();
+        //_statusTxt.text = "Joined as Host";
     }
 
     public void JoinClient()
     {
-        NetworkManager.Singleton.StartClient();
-        _statusTxt.text = "Joined as Client";
+        if (!_clientAuthenticated)
+        {
+            Debug.Log("Client not Authenticated. Please Try Again");
+            return;
+        }
+
+        if (_joinCodeText.text.Length <= 0)
+        {
+            Debug.Log($"Please Enter a Proper Join Code");
+            _statusTxt.text = "Please Enter a Proper Join Code";
+        }
+
+        Debug.Log($"The join code is: {_joinCodeText.text}");
+        StartCoroutine(ConfigureUseCodeAndJoinClient(_joinCodeText.text));
+
+        //UIUpdates
+        _btnClient.gameObject.SetActive(false);
+        _btnHost.gameObject.SetActive(false);
+        _joinCodeText.gameObject.SetActive(false);
+
+        //NetworkManager.Singleton.StartClient();
+        //_statusTxt.text = "Joined as Client";
     }
 
     public void JoinServer()
     {
+        if (!_clientAuthenticated)
+        {
+            Debug.Log("Client not Authenticated. Please Try Again");
+            return;
+        }
+
         NetworkManager.Singleton.StartServer();
         _statusTxt.text = "Joined as Server";
     }
